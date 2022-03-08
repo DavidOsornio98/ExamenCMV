8f8b227d01a68cd0751dfdd3d28c40b69e517379
David Osornio Garcia <deividdg6273@gmail.com>
2022-03-07 23:32:35 -0600

    commit inicial

---
+++ Frontend/examen/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CrudClientes/>
+</template>
+
+<script>
+import CrudClientes from './pages/CrudClientes.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CrudClientes
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
